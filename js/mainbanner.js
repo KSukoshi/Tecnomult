0a5838775219f8ac1a00e32ed7223af50e2c9aaa
Lucas Eduardo <ksukoshidesu@live.com>
2019-04-22 11:38:25 -0300

    Update layout

---
+++ js/mainbanner.js
@@ -0,0 +1,13 @@
+function run(interval, frames) {
+    var int = 1;
+
+    function func() {
+        document.getElementById("landpage").className = "b"+int;
+        int++;
+        if(int === frames) { int = 1; }
+    }
+
+    var swap = window.setInterval(func, interval);
+}
+
+run(3000, 4); //milliseconds, frames
